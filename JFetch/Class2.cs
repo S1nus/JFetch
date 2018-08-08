ac31090cdb06b579387ff406d0fbdf6f9ef4997d
cohara1 <cohara1@DESKTOP-6LRMQ9E>
2018-08-08 00:30:30 -0400

    Made "Class2" which is copying my coworker's approach to this problem.

---
+++ JFetch/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JFetch {
+	class Class2 {
+		public static List<Dictionary<string, string>> table;
+
+		public static Task<int> JFetch(string url) 
+	}
+}
--- JFetch/Program.cs
+++ JFetch/Program.cs
@@ -12,43 +12,43 @@ namespace JFetch {
 	
 	class Program {
 
-		public static void DoWithResponse (HttpWebRequest request, object otherstuff, Action<HttpWebResponse, object> responseAction) {
-			Action wrapperAction = () => {
-				request.BeginGetResponse(new AsyncCallback((iar) => {
-					var response = (HttpWebResponse)((HttpWebRequest)iar.AsyncState).EndGetResponse(iar);
-					responseAction(response, otherstuff);
-				}), request);
-			};
-			wrapperAction.BeginInvoke(new AsyncCallback((iar) => {
-				var action = (Action)iar.AsyncState;
-				action.EndInvoke(iar);
-			}), wrapperAction);
-		}
+		//public static void DoWithResponse (HttpWebRequest request, object otherstuff, Action<HttpWebResponse, object> responseAction) {
+		//	Action wrapperAction = () => {
+		//		request.BeginGetResponse(new AsyncCallback((iar) => {
+		//			var response = (HttpWebResponse)((HttpWebRequest)iar.AsyncState).EndGetResponse(iar);
+		//			responseAction(response, otherstuff);
+		//		}), request);
+		//	};
+		//	wrapperAction.BeginInvoke(new AsyncCallback((iar) => {
+		//		var action = (Action)iar.AsyncState;
+		//		action.EndInvoke(iar);
+		//	}), wrapperAction);
+		//}
 
 		//public static void SetJsonDict(HttpWebResponse response, out object target) {
 		//	var body = new StreamReader(response.GetResponseStream()).ReadToEnd();
 		//	target = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(body);
 		//}
 
-		public static void JFetch(HttpWebRequest request, out object target) {
-			object toRet = null;
-			DoWithResponse(request, toRet, (response, otherstuff) => {
-				var body = new StreamReader(response.GetResponseStream()).ReadToEnd();
-				otherstuff = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(body);
-			});
-			target = toRet;
-			Console.WriteLine(toRet);
-		}
+		//public static void JFetch(HttpWebRequest request, out object target) {
+		//	object toRet = null;
+		//	DoWithResponse(request, toRet, (response, otherstuff) => {
+		//		var body = new StreamReader(response.GetResponseStream()).ReadToEnd();
+		//		otherstuff = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(body);
+		//	});
+		//	target = toRet;
+		//	Console.WriteLine(toRet);
+		//}
 
-		static void Main(string[] args) {
+		//static void Main(string[] args) {
 
-			//http://mysafeinfo.com/api/data?list=englishmonarchs&format=json
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json");
-			object table = null;
-			JFetch(request, out table);
-			Console.Read();
+		//	//http://mysafeinfo.com/api/data?list=englishmonarchs&format=json
+		//	HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json");
+		//	object table = null;
+		//	JFetch(request, out table);
+		//	Console.Read();
 
-		}
+		//}
 
 	}
 }
